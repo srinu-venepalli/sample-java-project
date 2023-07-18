5bdfa8ebbf88af65001713d99a44ffb735161633
svenepal <svenepal@opentext.com>
2023-07-18 14:56:07 +0530

    added HelloWorld first time

---
+++ src/sample/java/project/HelloWorld.java
@@ -0,0 +1,18 @@
+package sample.java.project;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+public class HelloWorld {
+
+    public static void main(final String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
